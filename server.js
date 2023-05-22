8657e8f6a7020299d78eb93802aba56167f97554
unknown <adam.day7973@gmail.com>
2023-05-22 18:32:48 +1000

    created routes js pages and updated controllers, db and models

--- server.js
+++ server.js
@@ -9,6 +9,32 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Sets up session and connect to our Sequelize db
+const sess = {
+    secret: 'Super secret secret',
+    // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+    cookie: {
+      // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+      maxAge: 60 * 60 * 1000,
+      // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+      httpOnly: true,
+      // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+      secure: false,
+      // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    // Sets up session store
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  
+  app.use(session(sess));
+  
+
+
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
 
@@ -23,5 +49,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`));
 });
