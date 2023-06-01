a1d30f655cad3c525cd262cdc0db3345517dbd8e
unknown <adam.day7973@gmail.com>
2023-06-01 18:35:34 +1000

    building models, routes and main handlebar page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const morgan = require('morgan');
 
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -31,7 +35,7 @@ const sess = {
     }),
   };
   
-  app.use(session(sess));
+app.use(session(sess));
   
 
 
@@ -42,6 +46,7 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
