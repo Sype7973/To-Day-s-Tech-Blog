927969ebf6df0d0fb5b8102ffa841dbc109e41f3
unknown <adam.day7973@gmail.com>
2023-06-04 19:21:16 +1000

    updated login user routes

--- controllers/api/userApiRoutes.js
+++ controllers/api/userApiRoutes.js
@@ -4,35 +4,35 @@ const { User, blogPost, Blogcomment } = require('../../models');
 
 // this page displays data if the user is logged in
 // get all blog posts for displaying on the dashboard plus comments
-router.get('/', async (req, res) => {
-    try { 
-        const blogPostData = await blogPost.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-                {
-                    model: Blogcomment,
-                    attributes: ['comment_body', 'date_created', 'user_id'],
-                    include: {
-                        model: User,
-                        attributes: ['username'],
-                    },
-                },
-            ],
-        });
-        const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
-        res.render('dashboard', {
+// router.get('/', async (req, res) => {
+//     try { 
+//         const blogPostData = await blogPost.findAll({
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username'],
+//                 },
+//                 {
+//                     model: Blogcomment,
+//                     attributes: ['comment_body', 'date_created', 'user_id'],
+//                     include: {
+//                         model: User,
+//                         attributes: ['username'],
+//                     },
+//                 },
+//             ],
+//         });
+//         const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+//         res.render('dashboard', {
 
-            blogPosts,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        console.log("error in dashboard route");
-        res.status(500).json(err);
-    }
-});
+//             blogPosts,
+//             logged_in: req.session.logged_in
+//         });
+//     } catch (err) {
+//         console.log("error in dashboard route");
+//         res.status(500).json(err);
+//     }
+// });
 // get blogpost by id and show comments
 // router.get('/:id', async (req, res) => {
 //     try {
@@ -64,57 +64,65 @@ router.get('/', async (req, res) => {
 
 
 
-// creates a user
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-            res.status(200).json(userData);
-        });
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
     }
-    catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-// logs in a user
-router.post('/login', async (req, res) => {
+  });
+  
+  router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { username: req.body.username } });
-        if (!userData) {
-            res.status(400).json({ message: 'Incorrect username or password, please try again' });
-            return;
-        }
-        const validPassword = await userData.checkPassword(req.body.password);
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect username or password, please try again' });
-            return;
-        }
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-            res.json({ user: userData, message: 'You are now logged in!' });
-        });
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
     }
-    catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-// logs out a user
-router.post('/logout', (req, res) => {
+  });
+  
+  router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
     }
-    else {
-        res.status(404).end();
-    }
-}
-);
+  });
+  
+  module.exports = router;
+  
 
 
 
--- public/js/login-signup.js
+++ public/js/login-signup.js
@@ -1,23 +1,54 @@
-// // sign up functionality for the login-signup.handlebars page
-// //
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page (list of all pets)
+        document.location.replace('/pets');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
 
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
-//     // collect the data from the sign up form
-//     const username = document.querySelector('#username-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-//     // if both fields have content, send the data to the API endpoint
-//     if (username && password) {
-//         const response = await fetch('/api/users/signup', {
-//             method: 'POST',
-//             body: JSON.stringify({ username, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-//         // if the sign up is successful, redirect to the dashboard page
-//         if (response.ok) {
-//             document.location.replace('/dashboard');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// }
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/pets');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
