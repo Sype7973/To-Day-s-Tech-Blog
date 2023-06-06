c46ee8c3aa9a917c49ba02b1dda39b83afa944ad
unknown <adam.day7973@gmail.com>
2023-06-06 16:28:34 +1000

    updated dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,13 @@
 // delete button functionality for dashboard handlebars removes is-hidden class from delete button if it belongs to the user
 const deleteBlogPost = async (event) => {
+    const isBlogOwner = event.target.getAttribute('data-isBlogOwner');
+    if (isBlogOwner === 'true') {
+        document.querySelector('#deleteBlogPostbtn').classList.remove('is-hidden');
+    }
+    else {
+        document.querySelector('#deleteBlogPostbtn').classList.add('is-hidden');
+    }
+    
     const blogPostId = event.target.getAttribute('data-blogpost-id');
     const response = await fetch('/api/blogposts/' + blogPostId, {
         method: 'DELETE',
