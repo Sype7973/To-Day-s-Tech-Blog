422ca57fe6783f94344fa7f2a463d0f74a44e6ed
unknown <adam.day7973@gmail.com>
2023-06-06 22:52:46 +1000

    updated front end

--- public/js/editComment.js
+++ public/js/editComment.js
@@ -1,28 +1,31 @@
 // // handle submit button for editing comment on editComment.handlebars
-const updateCommentHandler = async (event) => {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+  // handle submit button for editing comment on editComment.handlebars
+  const updateCommentHandler = async (event) => {
+    event.preventDefault();
 
-  const content = document.querySelector('#edit-comment').value.trim();
-  const commentId = document.querySelector('#commentId').getAttribute('data-comment-id');
-  console.log(content, commentId);
-  if (content && commentId) {
-    try {
-      const response = await fetch(`/api/comments/${commentId}`, {
-        method: 'PUT',
-        // does this need to be title, body from models?
-        body: JSON.stringify({ content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+    const content = document.querySelector('#edit-comment').value.trim();
+    const commentId = document.querySelector('#commentId').getAttribute('data-comment-id');
+    console.log(content, commentId);
+    
+    if (content && commentId) {
+      try {
+        const response = await fetch(`/api/comments/${commentId}`, {
+          method: 'PUT',
+          body: JSON.stringify({ content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
 
-      if (response.ok) {
-        window.history.back();
-      } else {
-        throw new Error(response.statusText);
+        if (response.ok) {
+          window.history.back();
+        } else {
+          throw new Error(response.statusText);
+        }
+      } catch (err) {
+        alert(err.message);
       }
-    } catch (err) {
-      alert(err.message);
     }
-  }
-};
+  };
 
-document.querySelector('#updateCommentbtn').addEventListener('click', updateCommentHandler);
+  document.querySelector('#updateCommentbtn').addEventListener('click', updateCommentHandler);
+});
