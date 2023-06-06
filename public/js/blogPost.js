faadf7cd28986c397f97b0deaf5dc513f790169a
unknown <adam.day7973@gmail.com>
2023-06-06 13:47:33 +1000

    updated handlebar.js

--- public/js/blogPost.js
+++ public/js/blogPost.js
@@ -1,98 +1,103 @@
 const commentFormHandler = async (event) => {
-    console.log('commentFormHandler')
     event.preventDefault();
-
+  
     // Collect values from the comment form
     const comment_body = document.querySelector('#new-comment').value.trim();
+    const user_id = document.querySelector('#user-id').dataset.userId;
+    const blogPost_id = document.querySelector('#blog-title').dataset.blogId;
+  
     if (comment_body) {
-        const userID_El = document.querySelector('#user-id');
-        const user_id = userID_El.getAttribute('data-user-id');;
-
-        const postID_el = document.querySelector('#blog-title');
-        var blogPost_id = postID_el.getAttribute('data-blog-id');
-
+      try {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/comments/', {
-            method: 'POST',
-            body: JSON.stringify({ comment_body, user_id, blogPost_id }),
-            headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({ comment_body, user_id, blogPost_id }),
+          headers: { 'Content-Type': 'application/json' },
         });
-
+  
         if (response.ok) {
-            // If successful, refresh to view comment
-            document.location.reload();
+          // If successful, refresh to view comment
+          document.location.reload();
         } else {
-            alert(response.statusText);
+          throw new Error(response.statusText);
         }
+      } catch (err) {
+        alert(err.message);
+      }
     }
-};
-
-document
-    .querySelector('.comment-form')
-    .addEventListener('submit', commentFormHandler);
-
-
-const blogID = document.querySelector('#username').getAttribute('data-blog-owner');
-const userID = document.querySelector('#delete-blog').getAttribute('data-user-id');
-
-if (blogID === userID) {
-    document.querySelector('#delete-blog').classList.remove('hide');
-} else {
-    document.querySelector('#delete-blog').classList.add('hide');
-}
-
-document.querySelector('#delete-blog').addEventListener('click', async (event) => {
-
-    event.preventDefault();
-
-    const blogID = document.querySelector('#blog-title').getAttribute('data-blog-id');
-
-    const response = await fetch(`/api/blogPost/${blogID}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-        // If successful, refresh to view comment
-        document.location.replace(`/blogPost/${blogID}`);
+  };
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  
+  const deleteBlogButton = document.querySelector('#delete-blog');
+  
+  if (deleteBlogButton) {
+    const blogID = document.querySelector('#blog-title').dataset.blogId;
+    const userID = deleteBlogButton.dataset.userId;
+  
+    if (blogID === userID) {
+      deleteBlogButton.classList.remove('hide');
     } else {
-        alert(response.statusText);
+      deleteBlogButton.classList.add('hide');
     }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
+  
+    deleteBlogButton.addEventListener('click', async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await fetch(`/api/blogPost/${blogID}`, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          // If successful, refresh to view comment
+          document.location.replace(`/blogPost/${blogID}`);
+        } else {
+          throw new Error(response.statusText);
+        }
+      } catch (err) {
+        alert(err.message);
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
     const deleteButtons = document.querySelectorAll('.comment-delete');
-    const userID = document.querySelector('#delete-blog').getAttribute('data-user-id');
-
+    const userID = deleteBlogButton ? deleteBlogButton.dataset.userId : null;
+  
     deleteButtons.forEach(button => {
-        const commentOwner = button.getAttribute('data-comment-user');
-        console.log('commentOwner', commentOwner)
-        console.log('userID', userID)
-        if (commentOwner === userID) {
-            button.classList.remove('is-hidden');
-        } else {
-            button.classList.add('is-hidden');
-        }
+      const commentOwner = button.dataset.commentUser;
+      if (commentOwner === userID) {
+        button.classList.remove('is-hidden');
+      } else {
+        button.classList.add('is-hidden');
+      }
     });
-});
-
-const deleteButtons = document.querySelectorAll('.comment-delete');
-
-deleteButtons.forEach(button => {
-  button.addEventListener('click', async (event) => {
-    event.stopPropagation();
-
-    const commentID = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/comments/${commentID}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
   });
-});
+  
+  const deleteButtons = document.querySelectorAll('.comment-delete');
+  
+  deleteButtons.forEach(button => {
+    button.addEventListener('click', async (event) => {
+      event.stopPropagation();
+  
+      const commentID = event.target.dataset.id;
+  
+      try {
+        const response = await fetch(`/api/comments/${commentID}`, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          throw new Error(response.statusText);
+        }
+      } catch (err) {
+        alert(err.message);
+      }
+    });
+  });
+  
